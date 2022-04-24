0416210801df14e8be74b2f2863017f09713ba9e
osolio <102782754+osolio@users.noreply.github.com>
2022-04-24 01:49:39 -0400

    Add files via upload

---
+++ comfig.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = '5282678359:AAGqEGnl2aPMlFVeMbtjxJdZst-L7Yfn8rM'
+API_ID = '14681595'
+API_HASH = 'a86730aab5c59953c424abb4396d32d5'
+ADMIN_ID = [1180073584]
+
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 100
+
